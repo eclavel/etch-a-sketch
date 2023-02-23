e38e8e3ca6559555f6d8372e2be381758196c375
xxTokyoGhuol <71616872+xxTokyoGhuol@users.noreply.github.com>
2023-02-22 16:46:50 -0800

    testing between machines

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const div = document.createElement("div");
 const reset = document.querySelector("input.reset");
+const color = document.querySelector("input.color");
 
 function createGrid(size) {
   const grid = document.querySelector(".grid-container");
@@ -13,35 +14,32 @@ function createGrid(size) {
       const div = document.createElement("div");
       div.classList.add("square");
       grid.appendChild(div);
-      div.addEventListener("mouseover", ()=>{
-        div.style.backgroundColor="black"
+      div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
       });
     }
   }
 }
 
-function clearGrid(){
+function clearGrid() {
   const square = document.querySelectorAll(".square");
-  square.forEach(square => {square.style.backgroundColor = "white"});
-
+  square.forEach((square) => {
+    square.style.backgroundColor = "white";
+  });
 }
 
-function removeOldGrid(){
+function removeOldGrid() {
   const divs = document.querySelectorAll(".grid-container div");
-  divs.forEach(div => div.remove());
+  divs.forEach((div) => div.remove());
 }
 
 const slider = document.querySelector(".input .slider input");
 const output = document.querySelector(".input .slider output");
-slider.addEventListener('input',e=>{
+slider.addEventListener("input", (e) => {
   output.innerHTML = e.target.value + "x" + e.target.value;
   createGrid(e.target.value);
-})
+});
 
-reset.addEventListener('click',clearGrid);
+reset.addEventListener("click", clearGrid);
 
 createGrid(16);
-
-
-
-
